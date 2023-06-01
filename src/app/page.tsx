d81c37486b772e8233ef0d91745a2263dfc853c3
jorgetxt <89991735+caltamirano-links@users.noreply.github.com>
2023-06-01 17:47:40 -0500

    add parallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
 "use client";
+import Grid from "@mui/material/Grid";
 
 import Header from "./Header";
 import Slider1 from "./Slider1";
-import ImageSl1 from "../../public/sliders/developer_slide1.jpg";
-import Card from "./Card";
-
-import Grid from "@mui/material/Grid";
+// import ImageSl1 from "../../public/sliders/developer_slide1.jpg";
+// import Card from "./Card";
+import PortfolioScreen from "../components/PortFolioScreen";
+import TextSlider from "../components/TextSlider";
+import ParallaxImg from "../components/HorizontalParallax";
 
 export default function Home() {
   return (
     <>
       <Grid container>
-        <Grid item xs={12}>
+        <Grid item xs={12} style={{ zIndex: 2 }}>
           <Header />
         </Grid>
-
-        <Grid item xs={12} style={{ overflow: "hidden" }}>
-          <Slider1 />
+        <Grid item container xs={12} style={{ zIndex: 2, minHeight: "100vh" }}>
+          <Grid item xs={12} md={6}>
+            <TextSlider />
+          </Grid>
+          <Grid item xs={12} md={6} style={{ overflow: "hidden" }}>
+            <ParallaxImg />
+          </Grid>
         </Grid>
+
+        {/* <Grid item xs={12} style={{ overflow: "hidden", zIndex: 2 }}>
+          <Slider1 />
+        </Grid> */}
       </Grid>
 
       <Grid
@@ -25,10 +35,10 @@ export default function Home() {
         spacing={2}
         justifyContent="space-evenly"
         alignItems="center"
-        minHeight="60vh"
+        minHeight="100vh"
         padding={2}
       >
-        <Grid item>
+        {/* <Grid item style={{ zIndex: 2 }}>
           <Card
             image="sliders/developer_slide1.jpg"
             alt="test"
@@ -36,7 +46,7 @@ export default function Home() {
             title="Titulo"
           />
         </Grid>
-        <Grid item>
+        <Grid item style={{ zIndex: 2 }}>
           <Card
             image="sliders/developer_slide1.jpg"
             alt="test"
@@ -44,13 +54,16 @@ export default function Home() {
             title="Titulo"
           />
         </Grid>
-        <Grid item>
+        <Grid item style={{ zIndex: 2 }}>
           <Card
             image="sliders/developer_slide1.jpg"
             alt="test"
             description="descripcion"
             title="Titulo"
           />
+        </Grid> */}
+        <Grid item>
+          <PortfolioScreen />
         </Grid>
       </Grid>
     </>
---
+++ src/components/TextSlider.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useTrail, a } from "@react-spring/web";
+import Button from "@mui/material/Button";
+
+import styles from "./stylesText.module.css";
+import Stack from "@mui/material/Stack";
+
+const Trail: React.FC<{
+  open: boolean;
+  children: React.ReactNode;
+}> = ({ open, children }) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  });
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className={styles.trailsText} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  );
+};
+
+export default function App() {
+  const [open, set] = useState(true);
+  return (
+    <div className={styles.container} onClick={() => set((state) => !state)}>
+      <Trail open={open}>
+        <span>Hola</span>
+        <span>soy</span>
+        <span>Jorge</span>
+        <span>Sit</span>
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" color="secondary">
+            Descargar CV
+          </Button>
+          <Button variant="outlined">ver github</Button>
+        </Stack>
+      </Trail>
+    </div>
+  );
+}
